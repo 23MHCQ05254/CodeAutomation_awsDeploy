3ae2de860017a77febd8935b530786595ee8d65d
Joseph J <jangajoseph77@gmail.com>
2026-02-25 22:25:55 +0530

    commited

--- src/main/java/com/example/demo/CseController.java
+++ src/main/java/com/example/demo/CseController.java
@@ -1,14 +1,18 @@
 package com.example.demo;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CseController {
-	@GetMapping("/home")
-	public String rocks() {
-		return "Hi! this is Swetha";
-	}
-	
-	@GetMapping("/cse")
-	public String rocks1() {
-		return "Welcome to Third year cse";
-	}
+
+    @GetMapping("/home")
+    public String rocks() {
+        return "Hi! this is Joseph";
+    }
+
+    @GetMapping("/cse")
+    public String rocks1() {
+        return "Welcome to Third year cse";
+    }
 }
